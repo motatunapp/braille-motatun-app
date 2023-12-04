0a5432877f163b40e778ef69efa14d9211b9baed
motatunapp <152012854+motatunapp@users.noreply.github.com>
2023-12-04 16:37:46 +0800

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 const btnConfirm = document.querySelector('.confirm-button');
 
 btnConfirm.addEventListener('click', function() {
+  let inputData = document.querySelector(".input-form").value;
   let NAMA = document.querySelector('.name').value;
   let NIM = document.querySelector('.nomor-induk').value;
   let MATKUL = document.querySelector('.mata-kuliah').value;
@@ -9,22 +10,26 @@ btnConfirm.addEventListener('click', function() {
   var soundclk = new Audio("/src/click2.mp3");  
   soundclk.play()
   
-  let obj = {
-    nama: NAMA,
-    nim: NIM,
-    matkul: MATKUL,
-    userEmail: emailDosen
+  if (inputData === "") {
+    alert("Masukkan data terlebih dahulu!!");
+  } else {
+    let obj = {
+      nama: NAMA,
+      nim: NIM,
+      matkul: MATKUL,
+      userEmail: emailDosen,
+    };
+
+    fetch("/data", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    });
+
+    window.location = "/braille";
   }
-
-  fetch('/data', {
-    method: 'POST',
-    headers: {
-      "content-type": "application/json"
-    },
-    body: JSON.stringify(obj)
-  })
-
-  window.location = '/braille';
 
 })
 
